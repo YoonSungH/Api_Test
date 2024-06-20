6601b21a2778802b37ad213005d0cd120d24ea14
YoonSungH <skylife50@naver.com>
2024-06-20 14:46:18 +0900

    ,,
    ,,

--- src/main/java/com/example/api/controller/AuthController.java
+++ src/main/java/com/example/api/controller/AuthController.java
@@ -1,5 +1,34 @@
 package com.example.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("auth")
 public class AuthController {
+	
+	private final MembersService membersService;
+	private final JWTUtil jwtUtil;
+	
+	@PostMapping(value= "/join")
+	public ResponseEntity<Long> register(@RequestBody MembersDTO membersDTO){
+		log.info("register...." + membersDTO);
+		
+		long num = membersService.registMembersDTO(membersDTO);
+		return new ResponseEntity<>(num,HttpStatus.OK);
+	}
 
 }
