fb98b272494a846c163298e8ccfa47fecba1929c
YoonSungH <skylife50@naver.com>
2024-07-01 14:36:49 +0900

    AuthController
    logout

--- src/main/java/com/example/api/controller/AuthController.java
+++ src/main/java/com/example/api/controller/AuthController.java
@@ -2,14 +2,17 @@ package com.example.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.api.dto.ResponseDTO;
 
-
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -29,6 +32,16 @@ public class AuthController {
 		
 		long num = membersService.registMembersDTO(membersDTO);
 		return new ResponseEntity<>(num,HttpStatus.OK);
+	}
+	
+	@PostMapping("/logout")
+	public ResponseEntity<ResponseDTO> logout(HttpServletRequest request,HttpServletResponse response){
+		try {
+			new SecurityContextLogoutHandler().logout(request,response,SecurityContextHolder.getContext().getAuthentication());
+			return new ResponseEntity<>(new ResponseDTO("success",true),HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(new ResponseDTO(e.getMessage(), false), HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
